a40c26a3d4cc801b3f14437901ef7d9dd331adc9
yqbaowo <yqbaowo@qq.com>
2024-03-25 10:09:19 +0800

    ADD:pysqlcipher3

--- lib/crypto_sqlcipher.py
+++ lib/crypto_sqlcipher.py
@@ -2,15 +2,19 @@
 # -*- coding: UTF-8 -*-
 """
 @ Project     : QtLoginRegistration
-@ File        : crypto.py
+@ File        : crypto_sqlcipher.py
 @ Author      : yqbao
 @ Version     : V1.0.0
 @ Description : 将 sqlite3 替换为 pysqlcipher3,实现密码访问
                 pysqlcipher3 实现密码访问 SQLite，需要手动安装
                 安装看这里：https://www.cnblogs.com/yqbaowo/p/18043628
 """
+from os import getenv
+
 from pysqlcipher3 import dbapi2 as sqlite3
 import win32crypt
+
+db_password = getenv('DB_PASSWORD')  # 读取环境变量，数据库密码
 
 
 def crypto(password):
@@ -18,42 +22,42 @@ def crypto(password):
     return encrypt
 
 
-def create_db(db):
+def create_db(db, table):
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
-    cursor.execute("PRAGMA key='123456'")  # 密码=123456
+    cursor.execute(f"PRAGMA key='{db_password}'")  # 密码
     try:
-        cursor.execute(f'''CREATE TABLE {db} (username text , password text)''')
+        cursor.execute(f'CREATE TABLE {table} (username text , password text)')
         conn.commit()
     except sqlite3.OperationalError:
         pass
     conn.close()
 
 
-def insert_db(db, username, password):
+def insert_db(db, table, username, password):
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
-    cursor.execute("PRAGMA key='123456'")  # 密码=123456
+    cursor.execute(f"PRAGMA key='{db_password}'")  # 密码
     encrypt = crypto(password)
-    cursor.execute(f"INSERT INTO {db}(username,password) VALUES(?,?)", (username, encrypt))
+    cursor.execute(f"INSERT INTO {table}(username,password) VALUES(?,?)", (username, encrypt))
     conn.commit()
     conn.close()
 
 
-def delete_db(db):
+def delete_db(db, table):
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
-    cursor.execute("PRAGMA key='123456'")  # 密码=123456
-    cursor.execute(f"DELETE FROM {db}")
+    cursor.execute(f"PRAGMA key='{db_password}'")  # 密码
+    cursor.execute(f"DELETE FROM {table}")
     conn.commit()
     conn.close()
 
 
-def decrypt(db):
+def decrypt(db, table):
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
-    cursor.execute("PRAGMA key='123456'")  # 密码=123456
-    cursor.execute(f"SELECT * FROM {db}")
+    cursor.execute(f"PRAGMA key='{db_password}'")  # 密码
+    cursor.execute(f"SELECT * FROM {table}")
     result = cursor.fetchall()[-1]
     username = result[0]
     password = win32crypt.CryptUnprotectData(result[1])[-1]
@@ -62,8 +66,9 @@ def decrypt(db):
 
 
 if __name__ == '__main__':
-    db_ = 'test'
-    create_db(db_)
-    delete_db(db_)
-    insert_db(db_, 'test', '111111')
-    print(decrypt(db_))
+    db_ = 'test.db'
+    table_ = 'test'
+    create_db(db_, table_)
+    delete_db(db_, table_)
+    insert_db(db_, table_, 'test', '111111')
+    print(decrypt(db_, table_))
